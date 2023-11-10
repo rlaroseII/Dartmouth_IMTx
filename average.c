dff64bc9f18336d4dfd466916349dabc8fc1c472
rlaroseii <rlarose@tutanota.com>
2023-11-10 17:06:21 -0500

    Initial role values add properly during loop

--- average.c
+++ average.c
@@ -2,25 +2,31 @@
 
 int main(void) {
 
-	double red = 0; 	//initialize double
-	double yellow = 0;	//initialize double
-	double green = 0;	//initialize double
-	double cur_average = 0;		//initialize double
-	int num_of_rolls = 0;		//initialize int
-	int cur_roll = 0;		//initialize int
-	int total_rolls = 0;		//initialize int
+	double red = 0;
+	double yellow = 0;
+	double green = 0;
+	double cur_aver = 0;
+	double sum_roll = 0;
+	int num_of_rolls = 0;		
+	int cur_roll = 0;		
+	int total_rolls = 0;		
 	scanf("%d", &total_rolls);	//scan for total # of roll sets
 	for(int i = 0; i < total_rolls; i++){	//start for loop determining how many roll sets
 	scanf("%lf %lf %lf", &red, &yellow, &green);	//scan for each roll
+
 	num_of_rolls = num_of_rolls + 1;		//calculate number at beginning of printf statement
 	red = red * 1.0;				//declare value of red
 	yellow = yellow * 10.0;				//delcare value of yellow
 	green = green * 100.0;				//declare value of green
-	cur_roll = red + yellow + green;		//declare value of current roll set
-	cur_average = cur_roll + cur_roll / total_rolls;	//declare value of current average		
-//	red += green += yellow;
-	printf("%d. you rolled: %d, current average: %.1lf\n", num_of_rolls, cur_roll, cur_average);
+	cur_roll = red + yellow + green;		//declare value of current roll set	
+		
+//		for(int n = 0; n < total_rolls; n++) {
+//		cur_aver = cur_roll / num_of_rolls;	
+//		}
 
+
+	printf("%d. you rolled: %d, current average: %.1lf\n", num_of_rolls, cur_roll, cur_aver);
+	cur_roll += cur_roll;
 
 	}
 	return 0;
