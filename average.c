df6c7b847dbd5e7279afabb4f8397eca4171bb1c
rlaroseii <rlarose@tutanota.com>
2023-11-13 17:03:05 -0500

    added second for-loop providing temporary variable allowing for calculation of average

--- average.c
+++ average.c
@@ -6,29 +6,34 @@ int main(void) {
 	double yellow = 0;
 	double green = 0;
 	double cur_aver = 0;
-	double sum_roll = 0;
+	int sum_roll = 0;
 	int num_of_rolls = 0;		
 	int cur_roll = 0;		
 	int total_rolls = 0;		
-	scanf("%d", &total_rolls);	//scan for total # of roll sets
-	for(int i = 0; i < total_rolls; i++){	//start for loop determining how many roll sets
+	scanf("%d", &total_rolls);		//scan for total # of roll sets
+	for(int i = 0; i < total_rolls; i++){		//start for loop determining how many roll sets
 	scanf("%lf %lf %lf", &red, &yellow, &green);	//scan for each roll
 
-	num_of_rolls = num_of_rolls + 1;		//calculate number at beginning of printf statement
-	red = red * 1.0;				//declare value of red
-	yellow = yellow * 10.0;				//delcare value of yellow
-	green = green * 100.0;				//declare value of green
-	cur_roll = red + yellow + green;		//declare value of current roll set	
+	num_of_rolls += 1;			//calculate number at beginning of printf statement
+	red = red * 1.0;			//declare value of red
+	yellow = yellow * 10.0;			//declare value of yellow
+	green = green * 100.0;			//declare value of green
+	cur_roll = red + yellow + green;	//declare value of current roll set	
+		for(int n = 0; n < 1; n++){	//for loop iterating one time every iteration of first loop
+						//add initial value to new variable based on
+		sum_roll += cur_roll;		
+
+		}				//variable "cur_roll". After new value is given to 
+						//"sum_roll" it is then used to calculate the cur_aver
+						//based on it's temporary value provided by for-loop.
 		
-//		for(int n = 0; n < total_rolls; n++) {
-//		cur_aver = cur_roll / num_of_rolls;	
-//		}
 
-
+	cur_aver = sum_roll / num_of_rolls;	// After second for-loop "sum_roll" is able to be used for calculation of "cur_aver"
 	printf("%d. you rolled: %d, current average: %.1lf\n", num_of_rolls, cur_roll, cur_aver);
-	cur_roll += cur_roll;
 
 	}
+
+
 	return 0;
 
 
