623fd76781835a151deabc91509c6d0b0040d2ae
rlaroseii <rlarose@tutanota.com>
2024-01-19 17:58:36 -0500

    using a variable to assign itself a value using a function call

--- Dartmouth_IMTx DART.IMT.C.03/func_findint.c
+++ Dartmouth_IMTx DART.IMT.C.03/func_findint.c
@@ -5,32 +5,33 @@ int main(void){
 int i = 0;
 int num1 = 0;
 int num2 = 0;
+int num = 0;
 int num_int = 0;
-int s_int = 0;
-int swap = 0;
+int temp = 0;
+   
 
-    
-    scanf("%d", &num_int);
-    for(i=0; i<num_int / 2; i++){  
-       scanf("%d %d", &num1, &num2);
-       s_int = min(num1,num2);
-    }   
+  scanf("%d", &num_int);       //scan for number of inputs
 
-        
-    
-    printf("%d", swap);
-         
+  scanf("%d %d", &num1, &num2);    //scan for initial two inputs
+  temp = min(num1,num2);          //establish initial variable for comparison to rest of list
+  for(i=2; i<num_int; i++){      //loop through rest of inputs adding two to 'i' to account for first inputs 
+    scanf("%d", &num);           //scan for input
+    temp = min(num,temp);   //give variable the value of the function with the variable being used for input
+  }
+
+
+   printf("%d", temp);    
 
 return 0;
 
 }
 
-int min(int num1, int num2){
-
-
-   if(num1 < num2){
-       return num1;
-   }else{
-      return num2;
-   }
-} 
+int min(int num1, int num2){   //'min()' function used for comparing two values and returning the smaller one
+        
+  if(num1 < num2){
+  return num1;
+    }else{
+         return num2;
+         }
+  
+}
