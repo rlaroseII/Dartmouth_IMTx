846d0d21addf394ae76f9898ef3ee1f675138d89
rlaroseii <rlarose@tutanota.com>
2024-02-03 10:47:39 -0500

    Using expressions and if else statement to calculate user input

--- if_else.c
+++ if_else.c
@@ -4,13 +4,13 @@ int main(){
 	int pass = 0;
 	double cost_gas = 0;
 	double user_cost = 0;
-	scanf("%d %lf", &pass,&cost_gas);	//scan for int and double
-	if(pass > 0){				// if statement comparing int pass
-		cost_gas++;			// add 1 to variable
-		pass++;				// add 1 to variable
+	scanf("%d %lf", &pass,&cost_gas);     //scan for int and double
+	if(pass > 0){				        // if statement comparing int pass
+		cost_gas++;			        // add 1 to variable
+		pass++;				        // add 1 to variable
 		user_cost = cost_gas / pass;	//calculate user_cost
 		printf("%.2lf", user_cost);	//print new value of user_cost
-	}else{					//start of else statment
+	}else{					        //start of else statment
 		printf("%.2lf", cost_gas);	//print only original value of variable
 
 	}
