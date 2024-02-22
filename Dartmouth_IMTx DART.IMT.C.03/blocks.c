3cf37e7e4ff3aac6557b23ac4dbeccf7f2a2c3a5
rlaroseii <rlarose@tutanota.com>
2024-02-22 16:40:57 -0500

    program to help visualize code blocks

---
+++ Dartmouth_IMTx DART.IMT.C.03/blocks.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    //!showMemory(start=65520)
+    int a = 10;
+    int b = 11;
+
+    {
+      int c = 12;
+      int d = 13;
+      int e = a + c;
+      c = b + d;
+      printf("c: %d\n", c);
+      printf("d: %d\n", e);
+    }
+    int f = 14;
+    int g = 15;
+    printf("f: %d\n",f);
+    printf("g: %d", g);
+
+return 0;
+}
+
